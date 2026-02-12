936dcbdcdf8f310d86e6cfec592b21b5da744e57
Jefferson Luís <jeflsantos@hotmail.com>
2026-02-11 22:54:47 -0300

    Adiciona Script inicial de criacao da tabela clientes

--- 01_sql_basico/criar_tabela_clientes.sql
+++ 01_sql_basico/criar_tabela_clientes.sql
@@ -1,6 +1,35 @@
+-- =========================================
+-- Script: Criacao da tabela CLIENTES
+-- Autor: Jefferson Luis
+-- Objetivo: Estudo inicial de SQL
+-- =========================================
+
+
 CREATE TABLE clientes (
 	id NUMBER PRIMARY KEY,
-	nome VARCHAR2(50),
-	email VARCHAR2(100),
-	data_cadastro DATE
+	nome VARCHAR2(100),
+	email VARCHAR2(150),
+	data_cadastro DATE DEFAULT SYSDATE	
 );
+
+-- Inserindo dados de testes
+INSERT INTO clientes (id, nome, email)
+VALUES (1, 'Ana Silva','ana@email.com')
+
+INSERT INTO clientes (id, nome, email)
+VALUES (2, 'Carlos Souza','carlos@email.com')
+
+COMMIT;
+
+-- Consulta simples
+SELECT * FROM clientes;
+
+-- Consulta com filtro
+SELECT nome, email
+FROM clientes
+WHERER id = 1;
+
+-- Ordenação
+SELECT *
+FROM clientes 
+ORDER BY nome;
